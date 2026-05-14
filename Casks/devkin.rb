e7e6853c58faa68b49390c2245d6b213be00f249
LSH <104637774+lsh2613@users.noreply.github.com>
2026-05-14 16:54:57 +0900

    fix: sha256 추가

--- Casks/devkin.rb
+++ Casks/devkin.rb
@@ -2,17 +2,17 @@ cask "devkin" do
   version "0.1.0"
 
   on_arm do
-    sha256 "REPLACE_WITH_ARM64_DMG_SHA256"
+    sha256 "d387cfa98796e45ac665d3f7ec202666fd2e83659c3d6af11d24962ccb99be0c"
     url "https://github.com/lsh2613/devkin-release/releases/download/v#{version}/DevKin-#{version}-arm64.dmg"
   end
   on_intel do
-    sha256 "REPLACE_WITH_X64_DMG_SHA256"
+    sha256 "3b8cbbbf27335baba045be48f0436a9dc5e613265d0ef449a4b58dc972233ed3"
     url "https://github.com/lsh2613/devkin-release/releases/download/v#{version}/DevKin-#{version}-x64.dmg"
   end
 
   name "DevKin"
   desc "macOS toolbox for developers (JSON, Byte, Base64, JWT, SQL, Markdown, HTML, Text)"
-  homepage "https://github.com/lsh2613/devkin-release"
+  homepage "https://github.com/lsh2613/devkin-relase"
 
   auto_updates false
 
