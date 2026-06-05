ee6de7a3b9cc55b7f014705e6efb6dcc4a905c4b
LSH <lsh2613@naver.com>
2026-06-05 17:29:11 +0900

    release: v1.2.3

--- Casks/devkin.rb
+++ Casks/devkin.rb
@@ -1,12 +1,12 @@
 cask "devkin" do
-  version "1.2.2"
+  version "1.2.3"
 
   on_arm do
-    sha256 "1c7ec7324bce50db6f6e35c3a0d5aaa42ad6242f4548ad0785b62cc1e280ed45"
+    sha256 "ae9cef171f052b39c359ffca0fa001801231af442dc7dfc640d49f5c81512053"
     url "https://github.com/lsh2613/homebrew-devkin/releases/download/v#{version}/DevKin-#{version}-arm64.dmg"
   end
   on_intel do
-    sha256 "6a086b102475dfcf80552102ca8a62b9c2a1599bbf9efb415cafd7ed12643320"
+    sha256 "3c39fe54283ce5ac35374dbe23c223ab9a4fca63ed080c6d5500b205dd86780d"
     url "https://github.com/lsh2613/homebrew-devkin/releases/download/v#{version}/DevKin-#{version}-x64.dmg"
   end
 
